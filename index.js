b8cc62f87c3351cdee1443802f422e6f0817eda7
Monica-R <moniroka_339@hotmail.com>
2025-01-15 13:19:05 +0000

    done

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
-
+// Expected ouput: "Fred fed Ted bread and Ted fed Fred bread"
+const tongueTwister = `${s1} ${s2} ${s3} ${s4} ${s5} ${s3} ${s2} ${s1} ${s4}`;
 // Print out the concatenated string
-
+console.log(tongueTwister);
 
 
 
@@ -22,10 +22,9 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
-
+const result = `${part1.slice(0, -1)}${part1[part1.length - 1].toUpperCase()}${part2.slice(0, -1)}${part2[part2.length - 1].toUpperCase()}`;
 // Print the cameLtaiL-formatted string
-
+console.log(result);
 
 
 
@@ -33,12 +32,13 @@ const part2 = "script";
     Iteration 2.1 | Calculate Tip
 *******************************************/
 const billTotal = 84;
+const PERCENT = 15;
 
 // Calculate the tip (15% of the bill total)
-
+const tripAmount = billTotal * PERCENT / 100;
 
 // Print out the tipAmount
-
+console.log(tripAmount);
 
 
 
@@ -47,10 +47,12 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const min = 1;
+const max = 10;
+const randomNumber = Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min)) + Math.ceil(min));
 
 // Print the generated random number
-
+console.log(randomNumber);
 
 
 /*******************************************
@@ -62,15 +64,30 @@ const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
 const expression1 = a && b;
+// Expected expression: false 
 
 const expression2 = a || b;
+// Expected expression: false
 
 const expression3 = !a && b;
+// Expected expression: false
 
 const expression4 = !(a && b);
+// Expected expression: true
 
 const expression5 = !a || !b;
+// Expected expression: false
 
 const expression6 = !(a || b);
+// Expected expression: true
 
 const expression7 = a && a;
+// Expected expression: true
+
+console.log("Expression 1", expression1);
+console.log("Expression 2", expression2);
+console.log("Expression 3", expression3);
+console.log("Expression 4", expression4);
+console.log("Expression 5", expression5);
+console.log("Expression 6", expression6);
+console.log("Expression 7", expression7);
